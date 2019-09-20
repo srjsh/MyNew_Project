de6ade3c2a575957df2cb979b96d61d3acd5c65d
srjsh <32061866+srjsh@users.noreply.github.com>
2019-09-20 19:44:46 +0530

    updated successfull

--- main.java
+++ main.java
@@ -3,3 +3,6 @@ public static void main (String [] args)
 String = "This is my first project on the github"
 }
 System.out.println("Second file creadted");
+
+
+new branch is added other than master one.
